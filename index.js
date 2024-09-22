cd2a8e9cf62f9c679de003bcb47e65c809dc4820
Elia Albaba <ea152@uakron.edu>
2024-09-22 13:18:29 -0400

    Add node full stack feature

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { readFile } = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (request, response) => {
+
+    response.sendFile(path.join(__dirname, '/home.html'));
+
+    // readFile('./home.html', 'utf8', (err, html) => {
+    //     if (err) {
+    //         response.status(500).send('Sorry, we are facing some issues');
+    //     }
+    //     response.send(html);
+    // });
+});
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- makefile
+++ makefile
@@ -1,5 +1,19 @@
+installnvm:
+	# install nvm
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+installModules:
+	npm install express
+	npm install nodemon
+
+
 install:
+	# install node 18.18.1
+	nvm install 18.18.1
+	nvm use 18.18.1
+
 	npm install
+
 
 run:
 	wslview home.html
