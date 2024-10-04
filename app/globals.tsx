5e55f7914a31922114748168a4a648276beec90b
Elia Albaba <ea152@uakron.edu>
2024-10-03 22:06:55 -0400

    Globalize header and footer across the website

---
+++ app/globals.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+export const header = (
+    <>
+        <title>Home</title>
+        <link rel="stylesheet" href="globals.css" />
+        <header id="header" className="site-header">
+            <h1 className="site-title">JES - Financial Budgeting Tool</h1>
+            <nav className="site-navigation">
+                <ul className="nav-menu">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/budgets">Budgets</a>
+                </li>
+                <li>
+                    <a href="/transactions">Transactions</a>
+                </li>
+                </ul>
+            </nav>
+        </header>
+    </>
+);
+
+export const footer = (
+    <>
+        <footer>
+            <p>
+            © 2024 University of Akron. All Rights Reserved -- The Kernels (Javan
+            Miller, Elia Albaba, and Spencer Simms)
+            </p>
+        </footer>
+    </>
+);
