80afd4b65057c1258213223b2ca98ee94d2b0120
Elia Albaba <ea152@uakron.edu>
2024-10-25 14:33:10 -0400

    Delete any reference to nextUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 // tailwind.config.js
-const { nextui } = require('@nextui-org/theme');
 
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-    "./node_modules/@nextui-org/theme/dist/components/table.js",
   ],
   theme: {
     extend: {
@@ -18,45 +15,5 @@ module.exports = {
     },
   },
   darkMode: "class",
-  plugins: [
-    nextui({
-      themes: {
-        "purple-dark": {
-          extend: "dark", // Inherit default values from the dark theme
-          colors: {
-            background: "#F182F6",
-            foreground: "#ffffff",
-            primary: {
-              50: "#3B096C",
-              100: "#520F83",
-              200: "#7318A2",
-              300: "#9823C2",
-              400: "#c031e2",
-              500: "#DD62ED",
-              600: "#F182F6",
-              700: "#FCADF9",
-              800: "#FDD5F9",
-              900: "#FEECFE",
-              DEFAULT: "#F182F6",
-              foreground: "#ffffff",
-            },
-            focus: "#F182F6",
-          },
-          layout: {
-            disabledOpacity: "0.3",
-            radius: {
-              small: "4px",
-              medium: "6px",
-              large: "8px",
-            },
-            borderWidth: {
-              small: "1px",
-              medium: "2px",
-              large: "3px",
-            },
-          },
-        },
-      },
-    }),
-  ],
+  plugins: [],
 };
