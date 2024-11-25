31fb496c316951550ef1f2274e7d60e8e65c6691
Elia Albaba <ea152@uakron.edu>
2024-11-25 13:41:27 -0500

    Add Remaining column to the budgets table

--- pages/_API_Methods.tsx
+++ pages/_API_Methods.tsx
@@ -22,3 +22,23 @@ export const getCategoryTransactions = async (category: string) => {
     return null; // Return null or an empty array to handle the error case
     }
 };
+
+export const categorySum = async (category: string) => {
+    interface Transaction {
+        amount: number;
+    }
+
+    try {
+        const data = await getCategoryTransactions(category);
+        let sum = 0;
+        if (data && data.transactions) {
+            data.transactions.forEach((transaction: Transaction) => {
+                sum += transaction.amount;
+            });
+        }
+        return sum;
+    } catch (error) {
+        console.error("Error fetching category sum data:", error, "Category:", category);
+        return null; // Return null or an empty array to handle the error case
+    }
+};
